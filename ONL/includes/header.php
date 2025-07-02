b145d367a9fb7c0024b4a46aa586358686918d5e
Repelstelertje <andreasalaerts@protonmail.com>
2025-07-02 20:37:30 +0200

    aanpassen paden en bestanden

--- ONL/includes/header.php
+++ ONL/includes/header.php
@@ -139,7 +139,7 @@
     render_og_meta($og);
 ?>
 <!-- Bootstrap core CSS -->
-<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="css/vendor/bootstrap.min.css" rel="stylesheet">
 <!-- Custom styles for this template -->
 <link href="css/style.css" rel="stylesheet">
 </head>
