cbf1d29c033a88531279bb1cea18669196a134f3
Repelstelertje <andreasalaerts@protonmail.com>
2025-07-03 10:03:35 +0200

    Add sitemap generation scripts to all sites and document

---
+++ DC/generate_sitemap.php
@@ -0,0 +1,63 @@
+<?php
+require __DIR__ . '/includes/array_prov.php';
+require __DIR__ . '/includes/array_tips.php';
+$config = include __DIR__ . '/includes/config.php';
+
+$baseUrl = getenv('ONL_BASE_URL') ?: 'https://datingcontact.co.uk';
+
+require_once __DIR__ . '/includes/utils.php';
+
+$urls = [];
+
+$static = [
+    '/',
+    '/datingtips',
+    '/partnerlinks',
+    '/privacy',
+    '/cookie-policy',
+];
+foreach ($static as $path) {
+    $urls[] = $baseUrl . $path;
+}
+
+foreach (array_keys($provincies) as $slug) {
+    $urls[] = $baseUrl . "/dating-" . $slug;
+}
+foreach (array_keys($datingtips) as $slug) {
+    $urls[] = $baseUrl . "/datingtips-" . $slug;
+}
+
+$provinceApiBase = rtrim($config['BASE_API_URL'], '/') . '/profile/province';
+$countryMap = [ 'uk' => $provincies ];
+$profilePaths = [];
+foreach ($countryMap as $code => $provArr) {
+    foreach ($provArr as $prov) {
+        $endpoint = $provinceApiBase . '/' . $code . '/' . rawurlencode($prov['name']) . '/120';
+        $json = @file_get_contents($endpoint);
+        if ($json === false) continue;
+        $data = json_decode($json, true);
+        if (!$data || !isset($data['profiles'])) continue;
+        foreach ($data['profiles'] as $prof) {
+            if (empty($prof['id']) || empty($prof['name'])) continue;
+            $slug = slugify($prof['name']);
+            $profilePaths[$prof['id']] = $baseUrl . '/date-with-' . $slug . '?id=' . $prof['id'];
+        }
+    }
+}
+foreach ($profilePaths as $url) {
+    $urls[] = $url;
+}
+
+$xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><urlset></urlset>');
+$xml->addAttribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
+$xml->addAttribute('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
+$xml->addAttribute('xsi:schemaLocation', 'http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd');
+$lastMod = date('c');
+foreach ($urls as $loc) {
+    $url = $xml->addChild('url');
+    $url->addChild('loc', htmlspecialchars($loc, ENT_XML1));
+    $url->addChild('lastmod', $lastMod);
+}
+file_put_contents(__DIR__ . '/sitemap.xml', $xml->asXML());
+
+echo "Generated sitemap with " . count($urls) . " URLs\n";
