624971ad6e1cb0a0ea16a47c5ca135853cf9367b
Repelstelertje <andreasalaerts@protonmail.com>
2025-07-08 14:01:06 +0200

    ...

--- DC/includes/config.php
+++ DC/includes/config.php
@@ -6,8 +6,8 @@ return [
     'BASE_API_URL' => $api_url,
     // When APP_DEBUG is set to 'true', development error reporting is enabled
     'DEBUG' => getenv('APP_DEBUG') === 'true',
-    'BANNER_ENDPOINT' => $api_url . '/profile/banner9/120',
-    'PROFILE_ENDPOINT' => $api_url . '/profile/get0/9/',
+    'BANNER_ENDPOINT' => $api_url . '/profile/banner2/120',
+    'PROFILE_ENDPOINT' => $api_url . '/profile/get0/2/',
     'PROVINCE_ENDPOINT' => $api_url . '/profile/province/uk',
 ];
 ?>
