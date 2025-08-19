866c145d3564cd2d1673f04d42b4bad6dbb3fc1f
Repelstelertje <andreasalaerts@protonmail.com>
2025-08-19 15:32:11 +0200

    Add server-side profile index page

--- DN/includes/header.php
+++ DN/includes/header.php
@@ -36,6 +36,9 @@
 ?>
 <meta name="description" content="<?php echo htmlspecialchars($description, ENT_QUOTES, 'UTF-8'); ?>">
 <meta name="author" content="Dating Nebenan">
+<?php if (isset($metaRobots)): ?>
+<meta name="robots" content="<?php echo htmlspecialchars($metaRobots, ENT_QUOTES, 'UTF-8'); ?>">
+<?php endif; ?>
 <link rel="apple-touch-icon" sizes="180x180" href="img/fav/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="img/fav/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="img/fav/favicon-16x16.png">
